a428a348f527279c5ec0719b3df883b053e757e8
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-12 23:47:50 -0700

    Add connection to database

--- server/index.js
+++ server/index.js
@@ -9,3 +9,13 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
+
+// mongodb atlas
+const CONNECTION_URL = process.env.MONGODB_URL
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
