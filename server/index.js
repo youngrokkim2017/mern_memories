e8e0fea2e0e5178b23703eeb25e267f0304ab473
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-13 23:38:39 -0700

    Add get posts route

--- server/index.js
+++ server/index.js
@@ -4,7 +4,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts';
+
 const app = express();
+
+// routes
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
