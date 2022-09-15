6d01d4c38ad38cf199b6d72b62642c9a80d9cfe9
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-14 22:50:23 -0700

    Update components

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <h1>Form Page</h1>
+    )
+}
+
+export default Form;
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts = () => {
+    return (
+        <>
+            <h1>Posts Page</h1>
+            <Post />
+        </>
+    )
+}
+
+export default Posts;
