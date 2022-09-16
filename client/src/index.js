a3b40e0d3338f1593a66a978c2638a7f1b6b0122
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-15 23:29:19 -0700

    Initialize redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; // Provider keeps track of the store
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// initialize redux
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+)
