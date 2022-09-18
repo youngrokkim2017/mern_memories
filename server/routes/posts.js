8fea1e68e8557365a5c10def25948857e4ba8c1a
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-18 00:13:50 -0700

    Add post route for new posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
 // controllers
-import { getPosts, createPost } from '../controllers/posts.js'
+import { getPosts, createPost, updatePost } from '../controllers/posts.js'
 
 const router = express.Router();
 
 // localhost:5000/posts
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost)
 
 export default router;
