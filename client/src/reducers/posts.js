43ec4a934db6ae47d0cec60aef55f67fe1acd892
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-18 00:29:51 -0700

    Add update post redux state

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,6 +2,8 @@
 // based on action type, return the action or updated state
 export default (posts = [], action) => {
     switch (action.type) {
+        case 'UPDATE':
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post)
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
