8041996e64749fe575ab75d18bd9912ce967a781
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-20 23:42:49 -0700

    Update post styling

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -43,11 +43,19 @@ const Post = ({ post, setCurrentId }) => {
                         ))}
                     </Typography>
                 </div>
-                <Typography className={classes.title} variant='h5' gutterBottom>
+                <Typography 
+                    className={classes.title} 
+                    variant='h5' 
+                    gutterBottom
+                >
                     {post.title}
                 </Typography>
                 <CardContent>
-                    <Typography variant='h5' gutterBottom>
+                    <Typography 
+                        variant='body2' 
+                        color='textSecondary' 
+                        component='p' 
+                    >
                         {post.message}
                     </Typography>
                 </CardContent>
