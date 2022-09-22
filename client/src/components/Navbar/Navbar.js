5d5dd22a27ced926e56b1191d6f16750629c6f8f
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-21 23:59:56 -0700

    Add navbar component

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Typography } from "@material-ui/core";
+
+import useStyles from './styles';
+import memories from '../../images/memories.png';
+
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
+                    Memories
+                </Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
+            </div>
+        </AppBar>
+    )
+}
+
+export default Navbar
