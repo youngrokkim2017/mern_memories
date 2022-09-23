6a307f899a6a01054b4c430c98550479f1c2cb55
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-23 00:06:34 -0700

    Add react router dom routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container } from "@material-ui/core";
 
-import useStyles from './styles';
-import memories from './images/memories.png';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
 import Navbar from './components/Navbar/Navbar';
-import { getPosts } from './actions/posts';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(null);
-
-    useEffect(() => {
-        dispatch(getPosts);
-    }, [currentId, dispatch])
-
     return (
-        <Container maxwidth="lg">
-            <Navbar />
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxwidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth" exact component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     )
 }
 
