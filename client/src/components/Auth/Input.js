dc4c3f2a733cbd9992777f5e67aee41f94ade14f
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-27 23:44:01 -0700

    Update input ternary

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -15,7 +15,7 @@ const Input = ({ half, name, label, handleChange, handleShowPassword, autoFocus,
                 label={label}
                 autoFocus={autoFocus}
                 type={type}
-                InputProps={name === 'password' && {
+                InputProps={name === 'password' ? {
                     endAdornment: (
                         <InputAdornment position="end">
                             <IconButton onClick={handleShowPassword}>
@@ -23,7 +23,7 @@ const Input = ({ half, name, label, handleChange, handleShowPassword, autoFocus,
                             </IconButton>
                         </InputAdornment>
                     )
-                }}
+                } : null}
             />
         </Grid>
     )
