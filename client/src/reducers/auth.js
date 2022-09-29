6c27b90f096e11df2f0011246b04d364c3472c34
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-28 23:06:11 -0700

    Update logout case

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,6 +6,9 @@ const authReducer = (state = { authData: null }, action) => {
             // console.log(action?.data);
             localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
             return {...state, authData: action?.data };
+        case LOGOUT: 
+            localStorage.clear();
+            return { ...state, authData: null }
         default:
             return state;
     }
