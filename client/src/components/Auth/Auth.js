d370de0773b86856ba3ea7409e9ee27dfe83b853
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-03 23:34:45 -0700

    Update onchange and onsubmit handlers

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,20 +8,38 @@ import { useHistory } from 'react-router-dom';
 import useStyles from './styles';
 import Input from './Input';
 import Icon from './icon';
+import { signin, signup } from '../../actions/auth';
+
+const initialState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+}
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignUp] = useState(false);
+    const [formData, setFormData] = useState(initialState)
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        if (isSignup) {
+            // sign up
+            dispatch(signup(formData, history))
+        } else {
+            // sign in
+            dispatch(signin(formData, history))
+        }
     }
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
     const handleShowPassword = () => {
@@ -83,8 +101,8 @@ const Auth = () => {
                                     half
                                 />
                                 <Input 
-                                    name='firstName'
-                                    label="First Name"
+                                    name='lastName'
+                                    label="Last Name"
                                     handleChange={handleChange}
                                     half
                                 />
