148c0335174e1e27da63141f8411ee1c6517e0b9
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-06 23:49:08 -0700

    Update axios routes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts'; // url pointing to the backend route
+// axios instance
+const API = axios.create({ baseURL: 'http://localhost:5000/' })
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+// const url = 'http://localhost:5000/posts'; // url pointing to the backend route
+
+export const fetchPosts = () => API.get(posts);
+export const createPost = (newPost) => API.post(posts, newPost);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${url}/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${url}/${id}`);
+export const likePost = (id) => API.patch(`/posts/${url}/${id}/likePost`);
