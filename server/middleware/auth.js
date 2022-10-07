afe30bf84aab8314e0894fbcf8830c7a6190fa13
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-07 00:15:08 -0700

    Fix authorization syntax error

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const auth = async (req, res, next) => {
     try {
         // check if token is valid
-        const token = req.headers.Authorization.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];
         // check if own token or google token
         const isCustomAuth = token.ength < 500;
 
