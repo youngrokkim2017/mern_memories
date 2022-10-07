6a47f3c556755e13f46b034b9874064ea9260105
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-07 00:33:41 -0700

    Add check for jwt expiration

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Typography, Toolbar } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
@@ -18,7 +19,13 @@ const Navbar = () => {
         const token = user?.token;
 
         //  check for jwt
+        if (token) {
+            const decodedToken = decode(token);
 
+            if (decodedToken.exp * 1000 < new Date().getTime()) {
+                logout();
+            }
+        }
         
         // w/ google auth
         setUser(JSON.parse(localStorage.getItem('profile')));
