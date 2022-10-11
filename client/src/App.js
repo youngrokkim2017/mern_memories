5a5ec86a1110954b63fc2535a3f3af525a025bec
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-10 23:44:11 -0700

    Add redirect to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { Container } from "@material-ui/core";
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostItem from './components/PostItem/PostItem';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Container maxwidth="lg">
+            <Container maxwidth="xl">
                 <Navbar />
                 <Switch>
-                    <Route path="/" exact component={Home} />
+                    <Route path="/" exact component={() => <Redirect to="/posts" />} />
+                    <Route path="/posts" exact component={Home} />
+                    <Route path="/posts/search" exact component={Home} />
                     <Route path="/auth" exact component={Auth} />
                 </Switch>
             </Container>
