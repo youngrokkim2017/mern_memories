d7e7d6c25b6a21f28beafab89442ecea210e9f3f
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-11 23:53:22 -0700

    Add fields for search functionality

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,29 +1,96 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
 
+import useStyles from './styles'
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import { getPosts } from '../../actions/posts';
 import Paginate from '../Pagination';
 
+function useQuery() {
+    return new URLSearchParams(useLocation().search)
+}
+
 const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
     const classes = useStyles();
     const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(null);
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1; // read url and see if there is a page parameter
+    const searchQuery = query.get('searchQuery');
 
     useEffect(() => {
         dispatch(getPosts);
     }, [currentId, dispatch])
 
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13) {  // enter key code
+            // search for post
+            searchPost();
+        }
+    }
+
+    const handleAdd = (tag) => {
+        setTags([...tags, tag]);
+    }
+
+    const handleDelete = (tagToDelete) => {
+        setTags(tags.filter((tag) => tag !== tagToDelete))
+    }
+
+    const searchPost = () => {
+        if (search.trim()) {
+            // dispatch logic to fetch for search post with redux
+        } else {
+            history.push('/');
+        }
+    }
+
     return (
         <Grow in>
-            <Container>
-                <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
+            <Container maxWidth='x;'>
+                <Grid className={classes.gridContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={6} md={9}>
                         <Posts setCurrentId={setCurrentId} />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AppBar
+                            className={classes.appBarSearch} 
+                            position="static" 
+                            color="inherit"
+                        >
+                            <TextField 
+                                name="search"
+                                variant="outlined"
+                                label="Search"
+                                fullWidth
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                                onKeyPress={handleKeyPress}
+                            />
+                            <ChipInput 
+                                style={{ margin: '10px 0' }}
+                                value={tags}
+                                onAdd={handleAdd}
+                                onDelete={handleDelete}
+                                label="Search Tags"
+                                variant="outlined"
+                            />
+                            <Button
+                                className={classes.searchButton}
+                                onClick={searchPost}
+                                color="primary"
+                                variant="contained"
+                            >
+                                Search
+                            </Button>
+                        </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId}/>
                         <Paper className={classes.pagination} elevation={6}>
                             <Paginate />
