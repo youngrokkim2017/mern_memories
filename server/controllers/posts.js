996084886bbc131ff35cf8f334e303580c83f983
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-19 23:40:37 -0700

    Add getPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -33,6 +33,17 @@ export const getPostsBySearch = async (req, res) => {
     }
 }
 
+export const getPost = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
 export const createPost = async (req, res) => {
     // res.send('create new post')
     const post = req.body;
