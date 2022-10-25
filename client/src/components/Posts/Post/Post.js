19bb20a6c749a4b2b6a750f0ff5142d55d746e2a
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-25 00:24:36 -0700

    Update likes response time

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
@@ -15,14 +15,28 @@ const Post = ({ post, setCurrentId }) => {
     const dispatch = useDispatch();
     const history = useHistory();
     const user = JSON.parse(localStorage.getItem('profile'));
+    const [likes, setLikes] = useState(post?.likes);
+
+    const userId = user?.result?.googleId || user?.result?._id;
+    const hasLikedPost = post.likes.find((like) => like === (userId));
+
+    const handleLike = async () => {
+        dispatch(likePost(post._id));
+
+        if (hasLikedPost) {
+            setLikes(posts.likes.filter((id) => id !== (userId)));
+        } else {
+            setLikes([...post.likes, userId]);
+        }
+    }
 
     const Likes = () => {
-        if (post.likes.length > 0) {
-            return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+        if (likes.length > 0) {
+            return likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
                 ? (
-                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}` }</>
                 ) : (
-                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
                 );
         }
       
@@ -93,7 +107,7 @@ const Post = ({ post, setCurrentId }) => {
                     size='small'
                     color='primary'
                     disabled={!user?.result}
-                    onClick={() => dispatch(likePost(post._id))}
+                    onClick={handleLike}
                 >
                     {/* <ThumbUpAltIcon fontSize='small' /> */}
                     <Likes />
